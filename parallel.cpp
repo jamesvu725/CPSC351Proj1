ffd7d930f1a67e837db2fb956bdcc86da1927de6
jamesvu725 <35977136+jamesvu725@users.noreply.github.com>
2019-03-03 13:29:01 -0800

    Update parallel.cpp

--- parallel.cpp
+++ parallel.cpp
@@ -1,7 +1,7 @@
 #include <unistd.h>
-//#include <sys/types.h>
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <fstream>
 #include <vector>
